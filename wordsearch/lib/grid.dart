090ec42d27dcb060fb347b7a06efca574b82e4a1
a6you <austiny365@gmail.com>
2020-09-07 15:42:03 -0400

    Implement tile select and detecting found words

--- wordsearch/lib/grid.dart
+++ wordsearch/lib/grid.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 List<List<String>> gridState = [
   ['J', 'A', 'V', 'A', 'K', 'O', 'T', 'L', 'I', 'N'],
   ['O', 'B', 'J', 'E', 'C', 'T', 'I', 'V', 'E', 'C'],
   ['V', 'A', 'R', 'I', 'A', 'B', 'L', 'E', 'A', 'B'],
   ['M', 'O', 'B', 'I', 'L', 'E', 'F', 'T', 'S', 'D'],
-  ['L', 'G', 'T', 'Z', 'P', 'T', 'C', 'V', 'I', 'J'],
+  ['S', 'W', 'I', 'F', 'T', 'A', 'C', 'V', 'I', 'J'],
   ['S', 'V', 'W', 'U', 'Q', 'I', 'K', 'B', 'C', 'G'],
   ['P', 'E', 'O', 'X', 'T', 'J', 'S', 'O', 'W', 'Q'],
   ['D', 'F', 'Y', 'I', 'O', 'S', 'T', 'R', 'Z', 'L'],
@@ -15,14 +16,17 @@ List<List<String>> gridState = [
 
 List<List<String>> targetWords = [
   ['J', 'A', 'V', 'A'],
-  ['O', 'B', 'J', 'E', 'C', 'T', 'I', 'V', 'E', 'C'],
-  ['V', 'A', 'R', 'I', 'A', 'B', 'L', 'E'],
-  ['M', 'O', 'B', 'I', 'L', 'E'],
-  ['K', 'O', 'T', 'L', 'I', 'N'],
+  //'O', 'B', 'J', 'E', 'C', 'T', 'I', 'V', 'E', 'C'],
+  //['V', 'A', 'R', 'I', 'A', 'B', 'L', 'E'],
+  //['M', 'O', 'B', 'I', 'L', 'E'],
+  //['K', 'O', 'T', 'L', 'I', 'N'],
   ['S', 'W', 'I', 'F', 'T'],
 ];
 
 List<String> selectedLetters = [];
+
+int wordsFound = 0;
+String winMessage = "";
 
 class Board extends StatefulWidget {
   @override
@@ -60,6 +64,20 @@ class _State extends State<Board> {
           ),
         ),
       ),
+      Container(
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
+        decoration:
+            BoxDecoration(border: Border.all(color: Colors.black, width: 2.0)),
+        child: Text("Words Found: " + wordsFound.toString()),
+      ),
+      Container(
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
+        //decoration:
+        //BoxDecoration(border: Border.all(color: Colors.black, width: 2.0)),
+        child: Text(winMessage),
+      ),
     ]);
   }
 
@@ -68,27 +86,39 @@ class _State extends State<Board> {
     int x, y = 0;
     x = (index / gridStateLength).floor();
     y = (index % gridStateLength);
+
     return GestureDetector(
       onTap: () {
         _gridItemTapped(x, y);
-        Future.delayed(Duration(seconds: 1), () {});
+      },
+      onDoubleTap: () {
+        _selectionCheck(selectedLetters);
+        if (_winCheck()) {
+          print("You win!");
+        }
       },
       child: GridTile(
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 0.5)),
           child: Center(
-            child: _buildGridItem(x, y),
-          ),
+        child: Container(
+          child: _buildGridItem(x, y),
         ),
-      ),
+      )),
     );
   }
 
   Widget _buildGridItem(int x, int y) {
     if (gridState[x][y].contains('0')) {
       String letter = gridState[x][y].substring(0, 1);
-      return Container(color: Colors.blue, child: Text(letter));
+      return Container(
+          decoration: BoxDecoration(color: Colors.blue),
+          //color: Colors.blue,
+          child: Center(
+              child: Text(
+            letter,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          )));
     } else {
       return Text(gridState[x][y]);
     }
@@ -96,9 +126,55 @@ class _State extends State<Board> {
 
   void _gridItemTapped(int x, int y) {
     setState(() {
-      selectedLetters.add(gridState[x][y]);
-      print(selectedLetters.toString());
-      gridState[x][y] = gridState[x][y] + '0';
+      if (!gridState[x][y].contains('0')) {
+        selectedLetters.add(gridState[x][y]);
+        print(selectedLetters.toString());
+        gridState[x][y] = gridState[x][y] + '0';
+      }
     });
+  }
+
+  void _selectionCheck(List<String> selection) {
+    int gridStateLength = targetWords.length;
+    bool wordFound = false;
+
+    for (int i = 0; i < gridStateLength; i++) {
+      if (const IterableEquality().equals(selection, targetWords[i])) {
+        wordFound = true;
+      }
+    }
+    if (wordFound) {
+      print("Word found!");
+      setState(() {
+        wordsFound++;
+      });
+    } else {
+      print("Word not found!");
+      _unselectTiles(gridState);
+    }
+    selectedLetters = [];
+  }
+
+  void _unselectTiles(List<List<String>> grid) {
+    int targetWordsLength = targetWords.length;
+
+    for (int i = 0; i < targetWordsLength; i++) {
+      for (int j = 0; j < targetWordsLength; j++) {
+        if (gridState[i][j].contains('0')) {
+          gridState[i][j] = gridState[i][j].substring(0, 1);
+          _buildGridItem(i, j);
+        }
+      }
+    }
+  }
+
+  bool _winCheck() {
+    int targetWordsLength = targetWords.length;
+    if (targetWordsLength == wordsFound) {
+      setState(() {
+        winMessage = "You win!";
+      });
+    }
+    return targetWordsLength == wordsFound;
   }
 }
